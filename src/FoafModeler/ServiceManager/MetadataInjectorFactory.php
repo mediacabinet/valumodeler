8eea72c38ab69a5190cff43634df59d513b4eb39
Juha Suni <juha.suni@mediacabinet.fi>
2012-09-04 09:15:41 +0300

    Initializes ClassMetadataFactory and injects it to MetadataInjector

--- src/FoafModeler/ServiceManager/MetadataInjectorFactory.php
+++ src/FoafModeler/ServiceManager/MetadataInjectorFactory.php
@@ -1,6 +1,7 @@
 <?php
 namespace FoafModeler\ServiceManager;
 
+use FoafModeler\Doctrine\MongoDb\ClassMetadataFactory;
 use FoafModeler\Doctrine\MongoDb\MetadataInjector;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use Zend\ServiceManager\FactoryInterface;
@@ -11,13 +12,20 @@ class MetadataInjectorFactory
     public function createService(ServiceLocatorInterface $serviceLocator)
     {
         $dm     = $serviceLocator->get('FoafModelerDm');
+        $cache  = $serviceLocator->get('FoafModelerCache');
         $config = $serviceLocator->get('Configuration');
         $dir    = isset($config['foaf_modeler']['class_dir'])
             ? $config['foaf_modeler']['class_dir']
             : null;
         
-        $injector = new MetadataInjector($dm);
-        $injector->setDirectory($dir);
+        $factory = new ClassMetadataFactory();
+        $factory->setDirectory($dir);
+        
+        if($cache){
+            $factory->setCache($cache);
+        }
+        
+        $injector = new MetadataInjector($dm, $factory);
         return $injector;
     }
 }
