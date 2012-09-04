a903594d66d77e1b9d1b834f1304c0887c1c91dc
Juha Suni <juha.suni@mediacabinet.fi>
2012-09-04 09:14:47 +0300

    Injected event listener to flush metadata when ever a document changes

--- src/FoafModeler/ServiceManager/DocumentServiceFactory.php
+++ src/FoafModeler/ServiceManager/DocumentServiceFactory.php
@@ -13,6 +13,32 @@ class DocumentServiceFactory
         $dm = $serviceLocator->get('FoafModelerDm');
         $serviceBroker = $serviceLocator->get('ServiceBroker');
         
+        /**
+         * Attach listeners to flush class meta data when ever
+         * document changes
+         */
+        $serviceBroker->getEventManager()->attach(
+            array(
+                'post.Modeler.Document.create',
+                'post.Modeler.Document.update',
+                'post.Modeler.Document.remove',
+            ),
+            function($e) use ($serviceLocator, $dm){
+                $injector = $serviceLocator->get('FoafModelerMetadataInjector');
+                $name     = $e->getParam('name');
+                
+                if($name && $injector){
+                    $document = $dm->getRepository('FoafModeler\Model\Document')->findOneByName(
+                        $name
+                    );
+                    
+                    $injector->getFactory()->reloadClassMetadata(
+                        $document     
+                    );
+                }
+            }
+        );
+        
         $service = new Document($dm);
         return $service;
     }
