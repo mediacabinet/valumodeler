3e7d50f0afe09d065266c9a61dedba9c4aece478
Juha Suni <juha.suni@mediacabinet.fi>
2012-09-04 09:14:12 +0300

    Injected default input filters for models

--- src/FoafModeler/ServiceManager/DocumentManagerFactory.php
+++ src/FoafModeler/ServiceManager/DocumentManagerFactory.php
@@ -18,6 +18,8 @@ class DocumentManagerFactory
          */
         $config = $serviceLocator->get('Configuration');
         
+        $serviceBroker = $serviceLocator->get('ServiceBroker');
+        
         // Register field types and attach factory as default
         // fieldType factory for Documents
         $map = isset($config['foaf_modeler']['field_types'])
@@ -26,6 +28,23 @@ class DocumentManagerFactory
         
         $fieldTypeFactory = new FieldTypeFactory($map);
         Model\Field::setTypeFactory($fieldTypeFactory);
+        
+        // Set default input filters
+        Model\Document::setDefaultInputFilter(
+            $serviceBroker->service('InputFilter')->get('FoafModelerDocument')
+        );
+        
+        Model\Field::setDefaultInputFilter(
+            $serviceBroker->service('InputFilter')->get('FoafModelerField')
+        );
+        
+        Model\Embed::setDefaultInputFilter(
+            $serviceBroker->service('InputFilter')->get('FoafModelerEmbed')
+        );
+        
+        Model\Reference::setDefaultInputFilter(
+            $serviceBroker->service('InputFilter')->get('FoafModelerReference')
+        );
         
         // Create document manager instance
         $dm = parent::createService($serviceLocator);
