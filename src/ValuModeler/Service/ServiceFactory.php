37f127e9a39e6ecff93f139310d0b7e474f7d81e
Juha Suni <juha.suni@mediacabinet.fi>
2013-04-18 11:44:28 +0300

    ServiceFactory implementation

---
+++ src/ValuModeler/Service/ServiceFactory.php
@@ -0,0 +1,81 @@
+<?php
+namespace ValuModeler\Service;
+
+use ValuSo\Broker\ServiceBroker;
+
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\ServiceManager\AbstractFactoryInterface;
+
+class ServiceFactory implements AbstractFactoryInterface
+{
+    private $services = [
+        'valumodelerdocument'  => 'ValuModeler\Service\DocumentService',
+        'valumodelerembed'     => 'ValuModeler\Service\EmbedService',
+    ];
+    
+    private $initialized = [];
+    
+    /**
+     * Determine if we can create a service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return bool
+     */
+    public function canCreateServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        return isset($this->services[$name]);
+    }
+    
+    /**
+     * Create service with name
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @param $name
+     * @param $requestedName
+     * @return mixed
+    */
+    public function createServiceWithName(ServiceLocatorInterface $serviceLocator, $name, $requestedName)
+    {
+        $dm = $serviceLocator->get('doctrine.documentmanager.valu_modeler');
+        $serviceBroker = $serviceLocator->get('ServiceBroker');
+        
+        $this->initEvents($serviceLocator, $serviceBroker);
+        
+        $service = new $this->services[$name]($dm);
+        return $service;
+    }
+    
+    private function initEvents(ServiceLocatorInterface $serviceLocator, ServiceBroker $serviceBroker)
+    {
+        $brokerId = spl_object_hash($serviceBroker);
+        
+        if (in_array($brokerId, $this->initialized)) {
+            return false;
+        }
+        
+        $this->initialized[] = $brokerId;
+        
+        $serviceBroker->getEventManager()->attach(
+                array(
+                    'post.modeler.document.create',
+                    'post.modeler.document.update',
+                    'post.modeler.document.remove',
+                ),
+                function($e) use ($serviceLocator){
+                    $document = $e->getParam('document');
+                    
+                    if ($document) {
+                        $injector = $serviceLocator->get('ValuModelerMetadataInjector');
+                        
+                        $injector->getFactory()->reloadClassMetadata(
+                                $document
+                        );
+                    }
+                }
+        );
+        
+        return true;
+    }
+}
