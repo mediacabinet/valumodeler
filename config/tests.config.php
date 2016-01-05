9c7295e07a1288631fabd7109cb4441f6acd97f9
Juha Suni <juha.suni@mediacabinet.fi>
2016-01-05 15:47:04 +0200

    Configures test Mongo connection based on env variables

--- config/tests.config.php
+++ config/tests.config.php
@@ -1,6 +1,12 @@
 <?php
 return [
     'doctrine' => [
+        'connection' => [
+            'odm_default' => [
+                'server' => getenv('MONGO_SERVER'),
+                'port' => getenv('MONGO_PORT')
+            ]
+        ],
         'configuration' => [
             'odm_default' => [
                 'default_db' => 'valu_modeler_test'
